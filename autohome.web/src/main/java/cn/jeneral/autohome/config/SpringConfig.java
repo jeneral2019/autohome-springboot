79d57e4969947bf2d4f26a3cd24454a6046010e4
yuxiang.feng <yuxiang.feng@sqmall.com>
2021-12-01 11:21:49 +0800

    fix spring-boot start

---
+++ autohome.web/src/main/java/cn/jeneral/autohome/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package cn.jeneral.autohome.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:spring-cfg/application-servlet.xml")
+public class SpringConfig {
+}
